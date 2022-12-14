b83216186c3d5941c47565e196033459f1308bca
midasowo <kmiduch@gmail.com>
2022-12-14 20:15:56 +0100

    stopwatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public firstNumber = 0;
-  public secondNumber = 0;
-  public result: number = 0;
+  public seconds = 0;
+  public decSeconds = 0;
+
+  private intervalId: NodeJS.Timer | undefined;
   
-  public onClick(): void {
-    this.result = this.firstNumber + this.secondNumber;
+  public startStoper(): void {
+    this.intervalId = setInterval(() =>{
+      this.decSeconds++;
+      if(this.decSeconds === 10){
+        this.seconds++;
+        this.decSeconds = 0;
+      }
+    }, 100);
+  }
+
+  public stopStoper(): void {
+    if(this.intervalId)
+    clearInterval(this.intervalId);
+  }
+
+  public resetStoper(): void {
+    this.seconds = 0;
+    this.decSeconds = 0;
   }
 
 }
