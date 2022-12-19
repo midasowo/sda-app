37d2bdfd498182ee339297c6de14048d067a8d51
midasowo <kmiduch@gmail.com>
2022-12-19 17:52:58 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { StoperComponent } from './components/stoper/stoper.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 
 const routes: Routes = [
   {path: "stoper", component: StoperComponent},
-  {path: "userList", component: UsersListComponent},
-  {path: "", component: AppComponent}
+  {path: "usersList", component: UsersListComponent},
 ];
 
 @NgModule({
